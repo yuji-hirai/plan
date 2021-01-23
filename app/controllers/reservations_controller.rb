47b0c953086e92ee48e7ba0e7510c86bd51ac5b6
EC2 Default User <ec2-user@ip-172-31-28-43.ap-northeast-1.compute.internal>
2021-01-23 16:32:08 +0000

    完成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,26 +4,25 @@ class ReservationsController < ApplicationController
   def index
     @reservations = current_user.reservations.all
   end
+
   def new
     @reservation = Reservation.new
   end
-  
- 
-    
+
   def create
     @reservation = Reservation.new(reservation_params)
     if @reservation.save
       flash[:notice] = "ユーザーを新規登録しました"
       redirect_to reservations_path
     else
-      
+
       render "new"
     end
   end
-  
+
   def edit
   end
-  
+
   def destroy
     @reservation.destroy
     respond_to do |format|
@@ -31,11 +30,11 @@ class ReservationsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
   def set_room
     @room = Room.find(params[:id])
   end
-  
+
   def reservation_params
     params.require(:reservation).permit(:start_day, :end_day, :number, :room_id, :user_id)
   end
